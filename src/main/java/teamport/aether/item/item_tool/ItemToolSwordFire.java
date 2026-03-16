f93e1aef99c0f146d287834580cdf2c98d42a894
Jack6174 <jacksonminard7@gmail.com>
2026-03-15 21:40:31 -0400

    Fix item damage handling and change damage type

--- src/main/java/teamport/aether/item/item_tool/ItemToolSwordFire.java
+++ src/main/java/teamport/aether/item/item_tool/ItemToolSwordFire.java
@@ -22,6 +22,7 @@ public class ItemToolSwordFire extends ItemToolSword implements AetherHasCustomD
     @Override
     public boolean hitEntity(ItemStack itemstack, Mob target, Mob attacker) {
         boolean hitEntity = super.hitEntity(itemstack, target, attacker);
+        itemstack.damageItem(-1, player);
         if (target instanceof Mob && target.hurtTime == 10 && hitEntity) {
             if ((target instanceof Player) && ((Player) target).gamemode.isPlayerInvulnerable()) {
                 return false;
@@ -43,12 +44,12 @@ public class ItemToolSwordFire extends ItemToolSword implements AetherHasCustomD
         if (blockID != 0) return false;
         if (world.dimension != AetherDimension.getAether() && player != null && !world.setBlockWithNotify(blockX, blockY, blockZ, Blocks.FIRE.id())) return false;
         world.playSoundEffect(player, SoundCategory.WORLD_SOUNDS, (double) blockX + (double) 0.5F, (double) blockY + (double) 0.5F, (double) blockZ + (double) 0.5F, "fire.ignite", 1.0F, itemRand.nextFloat() * 0.4F + 0.8F);
-        itemstack.damageItem(1, player);
+        itemstack.damageItem(0, player);
         return true;
     }
 
     @Override
     public DamageType getDamageType(){
-        return DamageType.FIRE;
+        return DamageType.COMBAT;
     }
 }
